01fe97d27968b734d2c4293b0fbbc3679be035bf
Christian Bahnweg <cebahnweg@gmail.com>
2018-04-23 22:25:36 -0400

    adds initial code for project scaffolding

---
+++ index.js
@@ -0,0 +1,6 @@
+document.onload = initializeShell();
+function initializeShell () {
+  var wrapper = document.createElement('div');
+wrapper.setAttribute('id', 'gameShell');
+  document.getElementById('#gameShell').appendChild(document.createElement('p'));
+};
