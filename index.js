1ff6004ba6024bbcdc6edbc8990bde341689b994
Christian Bahnweg <cebahnweg@gmail.com>
2018-04-28 09:55:04 -0400

    wipes JS file for rebuild and skeletonizes HTML file

--- index.js
+++
@@ -1,6 +0,0 @@
-document.onload = initializeShell();
-function initializeShell () {
-  var wrapper = document.createElement('div');
-wrapper.setAttribute('id', 'gameShell');
-  document.getElementById('#gameShell').appendChild(document.createElement('p'));
-};
