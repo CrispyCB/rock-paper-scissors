ab2eef8b0760705dd37f56ab381ebc0be5a81f91
Christian Bahnweg <cebahnweg@gmail.com>
2018-04-30 17:45:24 -0400

    adds and configures playerScore and computerScore variables

--- index.js
+++ index.js
@@ -39,6 +39,8 @@
  let ComputerSelection;
  let PlayerSelection;
  let Select;
+ let playerScore;
+ let computerScore;
  function ComputerPlay(){
   var CompSelect = ["ROCK", "PAPER", "SCISSORS"];
   var rand = CompSelect[Math.floor(Math.random() * CompSelect.length)];
@@ -51,22 +53,30 @@ function PlayerChoice (Select){
   return PlayerSelection;
 }
 function round (ComputerSelection,PlayerSelection){
+playerScore = 0;
+computerScore = 0;
   if (ComputerSelection == "ROCK" && PlayerSelection == "SCISSORS"){
+    computerScore++;
     return "You lose! Rock beats Scissors!";
   }
   if (ComputerSelection == "ROCK" && PlayerSelection == "PAPER"){
+    playerScore++;
   return "You win! Paper beats Rock!";
   }
   if (ComputerSelection == "PAPER" && PlayerSelection == "SCISSORS"){
+    playerScore++;
     return "You win! Scissors beats Paper!" ;
   }
   if (ComputerSelection == "PAPER" && PlayerSelection == "ROCK"){
+    computerScore++;
     return "You lose! Paper beats Rock!";
   }
   if (ComputerSelection == "SCISSORS" && PlayerSelection == "PAPER"){
+    computerScore++;
     return "You lose! Scissors beats Paper!";
   }
   if (ComputerSelection == "SCISSORS" && PlayerSelection == "ROCK"){
+    playerScore++;
     return "You win! Rock beats Scissors!";
   }
   if (ComputerSelection == PlayerSelection) {
@@ -78,5 +88,6 @@ document.addEventListener('DOMContentLoaded', () => {
     document.querySelector("#pchoice").innerHTML = PlayerChoice(Select);
     document.querySelector("#cchoice").innerHTML = ComputerPlay(ComputerSelection);
     document.querySelector("#rdisplay").innerHTML = " " + round (ComputerSelection,PlayerSelection);
+    document.querySelector("#sdisplay").innerHTML = " " + playerScore;
    }
    startGame();});
