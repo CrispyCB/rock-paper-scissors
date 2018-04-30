a4d27c4b571c699e4e342a94f29449f1fcf27799
Christian Bahnweg <cebahnweg@gmail.com>
2018-04-30 18:27:01 -0400

    adds and configures rndCnt variable

--- index.js
+++ index.js
@@ -41,6 +41,7 @@
  let Select;
  let playerScore;
  let computerScore;
+ let rndCnt;
  function ComputerPlay(){
   var CompSelect = ["ROCK", "PAPER", "SCISSORS"];
   var rand = CompSelect[Math.floor(Math.random() * CompSelect.length)];
@@ -57,29 +58,36 @@ playerScore = 0;
 computerScore = 0;
   if (ComputerSelection == "ROCK" && PlayerSelection == "SCISSORS"){
     computerScore++;
+    rndCnt++;
     return "You lose! Rock beats Scissors!";
   }
   if (ComputerSelection == "ROCK" && PlayerSelection == "PAPER"){
     playerScore++;
+    rndCnt++;
   return "You win! Paper beats Rock!";
   }
   if (ComputerSelection == "PAPER" && PlayerSelection == "SCISSORS"){
     playerScore++;
+    rndCnt++;
     return "You win! Scissors beats Paper!" ;
   }
   if (ComputerSelection == "PAPER" && PlayerSelection == "ROCK"){
     computerScore++;
+    rndCnt++;
     return "You lose! Paper beats Rock!";
   }
   if (ComputerSelection == "SCISSORS" && PlayerSelection == "PAPER"){
     computerScore++;
+    rndCnt++;
     return "You lose! Scissors beats Paper!";
   }
   if (ComputerSelection == "SCISSORS" && PlayerSelection == "ROCK"){
     playerScore++;
+    rndCnt++;
     return "You win! Rock beats Scissors!";
   }
   if (ComputerSelection == PlayerSelection) {
+    rndCnt++;
     return "It's a tie!"
   }
 }
@@ -90,5 +98,6 @@ document.addEventListener('DOMContentLoaded', () => {
     document.querySelector("#rdisplay").innerHTML = " " + round (ComputerSelection,PlayerSelection);
     document.querySelector("#sdisplay").innerHTML = " " + playerScore;
     document.querySelector("#cdisplay").innerHTML = " " + computerScore;
+    document.querySelector("#round").innerHTML = " " + rndCnt;
   }
    startGame();});
